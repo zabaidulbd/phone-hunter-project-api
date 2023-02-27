0aec2abfa1deeffae7b5ba3ef5cc5a83097e94db
zabaidulbd <zislam5622@gmail.com>
2023-02-27 12:08:32 +0600

    add phone image

--- js/app.js
+++ js/app.js
@@ -12,7 +12,19 @@ const loadPhones = async () => {
 const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container');
     phones.forEach(phone => {
-
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card p-4">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in
+                    to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        phoneContainer.appendChild(phoneDiv);
     });
 }
 
